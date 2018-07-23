e9c853a444d17ad5a33d5d2dd3c09f179c1ea839
Scott Weinstein <sweinstein27@gmail.com>
2018-07-23 14:51:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Scott"
+  end
+
+  get '/hometown' do
+    "My hometown is Commack"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is gold skies"
+  end
 end
